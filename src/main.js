1603914482fb425a7d96a833ca92fde35b14f6f1
Ankit <misra.ankit@outlook.com>
2020-07-30 12:33:20 +0530

    initial

--- src/main.js
+++ src/main.js
@@ -22,18 +22,37 @@ function debounce(func, wait, immediate) {
 }
 
 function paintSettings() {
-	document.getElementById('text-input-id-0').value = json_loc;
+	document.getElementById('text-input-id-0').value = date;
 }
 
 function paintMap() {
-	json_loc = document.getElementById('text-input-id-0').value;
+	date_val = document.getElementById('text-input-id-0').value;
 
-	bl1 = "%%[var @dataurl set @dataurl = HTTPGet(\"";
-	bl2 = "{{.datasource MSContent source = @dataurl type = variable}}{{.data}} { \"target\": \"@dataurl\",\"filter\": \"Contact_ID == [ID]\"}";
-	bl3 = "{{/data}} {{.datasource contacts type = nested}} {{.data}} {\"target\": \"MSContent.content\"} {{/data}}";
-	bl4 = "{{#if Contact_ID == [ID]}} <img src =\"{{url}}\"> {{/if}} {{/datasource}} {{/datasource}}";
+	// Update the count down every 1 second
+	var x = setInterval(function() {
 
-	content = bl1 + json_loc + "\")]%% " + bl2 + bl3 + bl4;
+	// Get today's date and time
+	var now = new Date().getTime();
+	  
+	// Find the distance between now and the count down date
+	var distance = date_val - now;
+	  
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	  
+	// Output the result in an element with id="demo"
+	content = days + "d " + hours + "h "
+	+ minutes + "m " + seconds + "s ";
+	  
+	// If the count down is over, write some text 
+	if (distance < 0) {
+	  clearInterval(x);
+	  document.getElementById("demo").innerHTML = "EXPIRED";
+	}
+  }, 1000);
 
 	default_content = "<p><h4><b>Content Builder SDK</b></p>";
 	sdk.setSuperContent(default_content, (newSuperContent) => {});
